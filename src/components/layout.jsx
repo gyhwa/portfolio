367a9108eeca6d533da47bf5bb068f34c04c2f11
gyhwa <ginahwng@gmail.com>
2020-09-05 16:31:14 -0700

    Remove SEO for now

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { MDXProvider } from "@mdx-js/react"
 import Header from './partials/header';
 import Footer from './partials/footer';
-import SEO from './seo';
 import { Container, Row } from 'react-bootstrap';
 
 const shortcodes = Header;
@@ -10,7 +9,6 @@ const shortcodes = Header;
 export default function Layout({ children }) {
   return (
     <div>
-      <SEO />
       <Header />
         <MDXProvider components={shortcodes}>
          {children}
