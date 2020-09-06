c8ec0d5cee4f8a68b36d6905d9dcae233361c3e5
gyhwa <ginahwng@gmail.com>
2020-09-05 17:56:47 -0700

    Fix slug name

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       // Generated value based on filepath with "blog" prefix. you
       // don't need a separating "/" before the value because
       // createFilePath returns a path with the leading "/".
-      value: `${value}`,
+      value: `/portfolio${value}`,
     })
   }
 }
